7ac12727c5fee90697e329425999632814a70b89
Siddarth Sivakumar <siddarth.sivakumar@gmail.com>
2015-10-25 18:01:00 -0400

    Initial Commit

---
+++ BestOfTheBatchCheckIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BestOfTheBatchCheckIn
+//
+//  Created by Siddarth Sivakumar on 10/25/15.
+//  Copyright © 2015 Siddarth Sivakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
